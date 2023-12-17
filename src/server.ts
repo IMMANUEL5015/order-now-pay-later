b8f67ee6080db227d98df4ace26db927ab840514
Immanuel Diai <immanueldiai@gmail.com>
2023-12-17 10:29:38 +0100

    Begin Docker Setup

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: Number(process.env.PORT), host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: Number(process.env.PORT || 8080), host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
